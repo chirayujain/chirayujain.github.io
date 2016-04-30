e0281937b9db51156f6516fc965a397e29dfbcff
chirayujain <jain.chirayu98@gmail.com>
2016-04-30 15:49:09 -0400

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,9 +11,9 @@
     function showNextQuote() {
         ++quoteIndex;
         quotes.eq(quoteIndex % quotes.length)
-            .fadeIn(1500)
-            .delay(1500)
-            .fadeOut(1500, showNextQuote);
+            .fadeIn(1000)
+            .delay(1000)
+            .fadeOut(1000, showNextQuote);
     }
     
     showNextQuote();
