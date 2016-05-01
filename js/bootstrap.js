a8c0031ea6e7bac775ca66eb4c15d6deb3bc1555
chirayujain <jain.chirayu98@gmail.com>
2016-05-01 16:52:53 -0400

    update

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -8,7 +8,27 @@
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
  * ======================================================================== */
 
-
+function removeHoverCSSRule() {
+  if ('createTouch' in document) {
+    try {
+      var ignore = /:hover/;
+      for (var i = 0; i < document.styleSheets.length; i++) {
+        var sheet = document.styleSheets[i];
+        if (!sheet.cssRules) {
+          continue;
+        }
+        for (var j = sheet.cssRules.length - 1; j >= 0; j--) {
+          var rule = sheet.cssRules[j];
+          if (rule.type === CSSRule.STYLE_RULE && ignore.test(rule.selectorText)) {
+            sheet.deleteRule(j);
+          }
+        }
+      }
+    }
+    catch(e) {
+    }
+  }
+}
 +function ($) {
   'use strict';
 
