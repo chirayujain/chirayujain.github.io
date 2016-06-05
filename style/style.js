783b32f54861bfb1376f461d6d97f64f2722ebbd
chirayujain <jain.chirayu98@gmail.com>
2016-06-05 19:02:30 -0400

    update

---
+++ style/style.js
@@ -0,0 +1,16 @@
+(function() {
+
+    var quotes = $(".quotes");
+    var quoteIndex = -1;
+    
+    function showNextQuote() {
+        ++quoteIndex;
+        quotes.eq(quoteIndex % quotes.length)
+            .fadeIn(750)
+            .delay(750)
+            .fadeOut(750, showNextQuote);
+    }
+    
+    showNextQuote();
+    
+})();
